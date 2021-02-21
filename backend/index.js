b0f9a87b469343079c762af99d9edc707af90650
vishal jayapalan <vishal935@gmail.com>
2021-02-21 16:21:43 +0530

    basic structure

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+require('dotenv').config()
+
+const port = process.env.PORT || 5000
+
+app.use(cors())
+app.use(express.json())
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`)
+})
